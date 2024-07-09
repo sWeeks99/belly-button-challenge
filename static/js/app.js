fed833cc21b82722d013e0b07ffb73d51a3f0254
sWeeks99 <sean.weeks@outlook.com>
2024-07-09 16:25:14 -0700

    minor tweaks in main code

--- static/js/app.js
+++ static/js/app.js
@@ -3,13 +3,13 @@ function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // get the metadata field
-    let metadata_field = d3.select(".metadata");
+    let metadata_field = d3.select('.metadata');
 
     // Filter the metadata for the object with the desired sample number
     let metadata = data.metadata.filter(item => item.id == sample)[0];
         //
     // Use d3 to select the panel with id of `#sample-metadata`
-    let metadata_sample = d3.select("#sample-metadata");
+    let metadata_sample = d3.select('#sample-metadata');
 
     // Use `.html("") to clear any existing metadata
     metadata_sample.html("");
@@ -17,7 +17,7 @@ function buildMetadata(sample) {
     // Inside a loop, you will need to use d3 to append new
     // tags for each key-value in the filtered metadata.
     Object.entries(metadata).forEach(([key, value]) => {
-      metadata_sample.append("p").text(`${key}: ${value}`); //appends new tag for each [key, value] pair
+      metadata_sample.append('p').text(`${key}: ${value}`); //appends new tag for each [key, value] pair
     });
   });
 }
@@ -26,7 +26,7 @@ function buildMetadata(sample) {
 function buildCharts(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
     // Get the samples field
-    let samples_field = d3.select("#sample-metadata");  //same as metadata_sample, renamed to samples_field for organization
+    let samples_field = d3.select('#sample-metadata');  //same as metadata_sample, renamed to samples_field for organization
 
     // Filter the samples for the object with the desired sample number
     let metadata = data.metadata.filter(item => item.id == sample)[0];
@@ -42,17 +42,17 @@ function buildCharts(sample) {
       x: otu_ids,
       y: sample_values,
       text: otu_labels,
-      mode: "markers",
+      mode: 'markers',
       marker: {
         size: sample_values,
         color: otu_ids,
-        colorscale: "Earth" //colorscale "Earth" to fit with example on module page
+        colorscale: 'Earth' //colorscale "Earth" to fit with example on module page
       }
     };
     let bubble_layout = {
-      title: "Bacteria Cultures Per Sample",
-      xaxis: {title: "OTU ID" },
-      yaxis: {title: "Number of Bacteria"},
+      title: 'Bacteria Cultures Per Sample',
+      xaxis: {title: 'OTU ID' },
+      yaxis: {title: 'Number of Bacteria'},
       height: 500,
       width: 900
     };
@@ -67,12 +67,12 @@ function buildCharts(sample) {
       x: sample_values.slice(0, 10).reverse(), 
       y: otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
       text: otu_labels.slice(0, 10).reverse(),
-      type: "bar",
+      type: 'bar',
       orientation: 'h' // horizontal position like in module page
     };
     let bar_layout = {
-      title: "Top 10 Bacteria Cultures Found",
-      xaxis: {title: "Number of Bacteria"},
+      title: 'Top 10 Bacteria Cultures Found',
+      xaxis: {title: 'Number of Bacteria'},
       margin: {t: 30, l: 150}
     };
 
@@ -89,13 +89,13 @@ function init() {
     let names_field = d3.select('meta[name="viewport"]');
 
     // Use d3 to select the dropdown with id of '#selDataset'
-    let dropdown = d3.select("#selDataset");
+    let dropdown = d3.select('#selDataset');
 
     // Use the list of sample names to populate the select options
     // Hint: Inside a loop, you will need to use d3 to append a new
     // option for each sample name.
     data.names.forEach((name) => {
-      dropdown.append("option").text(name).property("value", name);
+      dropdown.append('option').text(name).property('value', name);
     });
 
     // Get the first sample from the list
